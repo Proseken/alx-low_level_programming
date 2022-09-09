c3ed83a259e0be688e822a3fbfd6221e38bf000e
Proseken <sundaymiracle0010@gmail.com>
2022-09-08 19:00:16 -0700

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Print "Programming is like building a multilingual puzzle"
+ * Return: 0 if exited properly, none zero otherwise
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
